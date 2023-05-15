f6f7919c09d5d7c9b09f7eb79e09e017ef69e943
“pravallika47 <pravallikakakarlam@gmail.com>
2023-05-15 12:22:31 +0530

    c-conversion

---
+++ conversion_programs/dec_to_binary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int n, bin=0, rem, i = 1;
+  printf("Enter a decimal number: ");
+  scanf("%d", &n);
+  
+  while (n!=0) 
+  {
+    rem = n % 2;
+    n /= 2;
+    bin += rem * i;
+    i *= 10;
+  }
+  printf("binary =  %d\n", bin);
+  return 0;
+}
